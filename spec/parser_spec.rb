a6571ed1824b3f5ec69e452d049929238f1cff08
hc5duke <hc5duke@gmail.com>
2010-02-04 22:55:23 -0800

    more parser tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -12,17 +12,17 @@ describe Parser do
 
   describe "when created" do
     it "should handle empty array of files" do
-      lambda{ @report = Parser.new([])              }.should_not raise_error
+      @report = Parser.new []
       @report.transactions.count.should == 0
     end
 
     it "should be able to open file" do
-      lambda{ @report = Parser.new(@good_files[0])  }.should_not raise_error
+      @report = Parser.new @good_files[0]
       @report.transactions.count.should == @count_good_files[0]
     end
 
     it "should be able to open multiple files" do
-      lambda{ @report = Parser.new(@good_files)     }.should_not raise_error
+      @report = Parser.new @good_files
       @report.transactions.count.should == @count_good_files[0] + @count_good_files[1]
     end
 
@@ -41,13 +41,30 @@ describe Parser do
     end
 
     it "should be able to add single file" do
-      lambda{ @report.add_file(@good_files[0])      }.should_not raise_error
+      @report.add_file @good_files[0]
       @report.transactions.count.should == @count_good_files[0]
     end
 
     it "should be able to add multiple files" do
-      lambda{ @report.add_files(@good_files)        }.should_not raise_error
+      @report.add_files @good_files
       @report.transactions.count.should == @count_good_files[0] + @count_good_files[1]
+    end
+  end
+
+  describe "when adding transaction" do
+    before(:each) do
+      @report = Parser.new
+      @good_transaction = "APPLE	US	000000001	 	 	Acme Corp	Widgets	 	1	1	0	1/21/10	1/21/10	USD	US	USD	 	 	 	350525324	0	 	 	 	 	CR-RW	 "
+      @bad_transaction = "APPLE	US	000000001	 	 	Acme Corp	Widgets	 	1	1	0	1/21/10	1/21/10	USD	US	USD"
+    end
+
+    it "should handle correctly formed transaction" do
+      @report.add_transaction @good_transaction
+      @report.transactions.count.should == 1
+    end
+
+    it "should handle incorrectly formed transaction" do
+      lambda { @report.add_transaction @bad_transaction }.should raise_error(WrongNumberOfElements)
     end
   end
 end
